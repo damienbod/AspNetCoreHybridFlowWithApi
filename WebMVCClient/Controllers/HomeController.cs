6f36556b79a3fd560bd5a310129750ab65de9e20
damienbod <damien_bod@hotmail.com>
2018-01-30 21:41:11 +0100

    Init Hybrid ASP.NET Core client

---
+++ WebMVCClient/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebHybridClient;
+using WebMVCClient.Models;
+
+namespace WebMVCClient.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public async System.Threading.Tasks.Task<IActionResult> Index()
+        {
+            var apiService = new ApiService();
+            var result = await apiService.GetApiDataAsync();
+
+            ViewData["data"] = result.ToString();
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
