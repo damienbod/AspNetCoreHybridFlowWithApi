345ea320b0cf05d3740f6a098fb521dbd265d5f1
damienbod <damien_bod@hotmail.com>
2018-02-07 21:15:17 +0100

    Adding security headers

--- WebMVCClient/Startup.cs
+++ WebMVCClient/Startup.cs
@@ -13,6 +13,7 @@ namespace WebMVCClient
 {
     public class Startup
     {
+        private string stsServer = "";
         public Startup(IConfiguration configuration)
         {
             //Log.Logger = new LoggerConfiguration()
@@ -35,7 +36,7 @@ namespace WebMVCClient
             services.Configure<AuthConfigurations>(Configuration.GetSection("AuthConfigurations"));
 
             var authConfigurations = Configuration.GetSection("AuthConfigurations");
-            var stsServer = authConfigurations["StsServer"];
+            stsServer = authConfigurations["StsServer"];
 
             services.AddAuthentication(options =>
             {
@@ -78,6 +79,22 @@ namespace WebMVCClient
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            //Registered before static files to always set header
+            app.UseHsts(hsts => hsts.MaxAge(365).IncludeSubdomains());
+            app.UseXContentTypeOptions();
+            app.UseReferrerPolicy(opts => opts.NoReferrer());
+            app.UseXXssProtection(options => options.EnabledWithBlockMode());
+            app.UseXfo(options => options.Deny());
+
+            app.UseCsp(opts => opts
+                .BlockAllMixedContent()
+                .StyleSources(s => s.Self())
+                .StyleSources(s => s.UnsafeInline())
+                .FontSources(s => s.Self())
+                .ImageSources(s => s.Self())
+                .ScriptSources(s => s.Self())
+            );
 
             app.UseStaticFiles();
 
