12be098630ba9ca1342e64c713173c3baf71b603
damienbod <damien_bod@hotmail.com>
2018-02-07 22:34:38 +0100

    Adding HTTP headers to the Web API

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -69,6 +69,22 @@ namespace WebApi
                 app.UseBrowserLink();
             }
 
+            //Registered before static files to always set header
+            app.UseHsts(hsts => hsts.MaxAge(365).IncludeSubdomains());
+            app.UseXContentTypeOptions();
+            app.UseReferrerPolicy(opts => opts.NoReferrer());
+            app.UseXXssProtection(options => options.EnabledWithBlockMode());
+            app.UseXfo(options => options.Deny());
+
+            app.UseCsp(opts => opts
+                .BlockAllMixedContent()
+                .StyleSources(s => s.Self())
+                .StyleSources(s => s.UnsafeInline())
+                .FontSources(s => s.Self())
+                .ImageSources(s => s.Self())
+                .ScriptSources(s => s.Self())
+            );
+
             app.UseStaticFiles();
 
             app.UseAuthentication();
