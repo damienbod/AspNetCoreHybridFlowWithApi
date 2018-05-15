26b1cab1b11ab9f42c315c5a61c0d4f27db62388
damienbod <damien_bod@hotmail.com>
2018-05-15 21:43:33 +0200

    updating to .NET Core 2.1

--- WebMVCClient/Startup.cs
+++ WebMVCClient/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
