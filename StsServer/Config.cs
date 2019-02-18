fa30a0bce2d28acd63df898978ac12aa821e3f7c
damienbod <damien_bod@hotmail.com>
2019-02-18 15:25:34 +0100

    updating client configurations

--- StsServer/Config.cs
+++ StsServer/Config.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
@@ -15,7 +16,8 @@ namespace StsServerIdentity
             {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile(),
-                new IdentityResources.Email()
+                new IdentityResources.Email(),
+                new IdentityResource("my_identity_scope",new []{ "role", "admin", "user" } )
             };
         }
 
@@ -23,67 +25,77 @@ namespace StsServerIdentity
         {
             return new List<ApiResource>
             {
-                // example code
-                //new ApiResource("dataEventRecords")
-                //{
-                //    ApiSecrets =
-                //    {
-                //        new Secret("dataEventRecordsSecret".Sha256())
-                //    },
-                //    Scopes =
-                //    {
-                //        new Scope
-                //        {
-                //            Name = "dataeventrecords",
-                //            DisplayName = "Scope for the dataEventRecords ApiResource"
-                //        }
-                //    },
-                //    UserClaims = { "role", "admin", "user", "dataEventRecords", "dataEventRecords.admin", "dataEventRecords.user" }
-                //}
+                new ApiResource("scope_used_for_hybrid_flow")
+                {
+                    ApiSecrets =
+                    {
+                        new Secret("hybrid_flow_secret".Sha256())
+                    },
+                    UserClaims = { "role", "admin", "user", "some_api" }
+                },
+                new ApiResource("ProtectedApi")
+                {
+                    DisplayName = "API protected",
+                    ApiSecrets =
+                    {
+                        new Secret("api_in_protected_zone_secret".Sha256())
+                    },
+                    Scopes =
+                    {
+                        new Scope
+                        {
+                            Name = "scope_used_for_api_in_protected_zone",
+                            ShowInDiscoveryDocument = false
+                        }
+                    },
+                    UserClaims = { "role", "admin", "user", "safe_zone_api" }
+                }
             };
         }
 
-        public static IEnumerable<Client> GetClients(IConfigurationSection stsConfig)
+        public static IEnumerable<Client> GetClients(IConfigurationSection authConfigurations)
         {
-            // TODO use configs in app
-            //var yourConfig = stsConfig["ClientUrl"];
+            var hybridClientUrl = authConfigurations["HybridClientUrl"];
 
             return new List<Client>
             {
-                // example code
-                //new Client
-                //{
-                //    ClientName = "angularclient",
-                //    ClientId = "angularclient",
-                //    AccessTokenType = AccessTokenType.Reference,
-                //    AccessTokenLifetime = 330,// 330 seconds, default 60 minutes
-                //    IdentityTokenLifetime = 30,
-                //    AllowedGrantTypes = GrantTypes.Implicit,
-                //    AllowAccessTokensViaBrowser = true,
-                //    RedirectUris = new List<string>
-                //    {
-                //        "https://localhost:44311",
-                //        "https://localhost:44311/silent-renew.html"
-
-                //    },
-                //    PostLogoutRedirectUris = new List<string>
-                //    {
-                //        "https://localhost:44311/unauthorized",
-                //        "https://localhost:44311"
-                //    },
-                //    AllowedCorsOrigins = new List<string>
-                //    {
-                //        "https://localhost:44311",
-                //        "http://localhost:44311"
-                //    },
-                //    AllowedScopes = new List<string>
-                //    {
-                //        "openid",
-                //        "role",
-                //        "profile",
-                //        "email"
-                //    }
-                //}
+                new Client
+                {
+                    ClientName = "hybridclient",
+                    ClientId = "hybridclient",
+                    ClientSecrets = {new Secret("hybrid_flow_secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    AllowOfflineAccess = true,
+                    RedirectUris = {
+                        "https://localhost:44329/signin-oidc",
+                        $"{hybridClientUrl}/signin-oidc"
+                    },
+                    PostLogoutRedirectUris = {
+                        "https://localhost:44329/signout-callback-oidc",
+                        $"{hybridClientUrl}/signout-callback-oidc"
+                    },
+                    AllowedCorsOrigins = new List<string>
+                    {
+                        "https://localhost:44329/",
+                        $"{hybridClientUrl}/"
+                    },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OfflineAccess,
+                        "scope_used_for_hybrid_flow",
+                        "role"
+                    }
+                },
+                new Client
+                {
+                    ClientId = "ProtectedApi",
+                    ClientName = "ProtectedApi",
+                    ClientSecrets = new List<Secret> { new Secret { Value = "api_in_protected_zone_secret".Sha256() } },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = new List<string> { "scope_used_for_api_in_protected_zone" }
+                }
             };
         }
     }
