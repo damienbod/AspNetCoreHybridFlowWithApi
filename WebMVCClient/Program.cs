62a517f2fb65aa22910b4b2b326e6c551f064ec4
damienbod <damien_bod@hotmail.com>
2019-04-30 16:17:33 +0200

    code clean up app start in program, add in process hosting

--- WebMVCClient/Program.cs
+++ WebMVCClient/Program.cs
@@ -19,7 +19,7 @@ namespace WebHybridClient
             try
             {
                 Log.Information("Starting web host");
-                BuildWebHost(args).Run();
+                CreateWebHostBuilder(args).Build().Run();
                 return 0;
             }
             catch (Exception ex)
@@ -33,20 +33,18 @@ namespace WebHybridClient
             }
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>()
-            .UseKestrel(c => c.AddServerHeader = false)
-            .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
-                .ReadFrom.Configuration(hostingContext.Configuration)
-                .Enrich.FromLogContext()
-                .WriteTo.File(
-                  $@"D:\home\LogFiles\Application\security-headers-sts-webhybridclient.txt",
-                  fileSizeLimitBytes: 1_000_000,
-                  rollOnFileSizeLimit: true,
-                  shared: true,
-                  flushToDiskInterval: TimeSpan.FromSeconds(1)))
-            .Build();
+                .UseStartup<Startup>()
+                .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+                    .ReadFrom.Configuration(hostingContext.Configuration)
+                    .Enrich.FromLogContext()
+                    .WriteTo.File(
+                      $@"D:\home\LogFiles\Application\security-headers-sts-webhybridclient.txt",
+                      fileSizeLimitBytes: 1_000_000,
+                      rollOnFileSizeLimit: true,
+                      shared: true,
+                      flushToDiskInterval: TimeSpan.FromSeconds(1)));
 
     }
 }
--- WebMVCClient/Startup.cs
+++ WebMVCClient/Startup.cs
@@ -6,8 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Serilog;
+
 
 namespace WebHybridClient
 {
