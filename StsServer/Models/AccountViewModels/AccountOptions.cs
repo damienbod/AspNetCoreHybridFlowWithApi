672342be4bf553075c57abdb63bcaa6d420688e5
damienbod <damien_bod@hotmail.com>
2020-07-03 15:48:23 +0200

    updating STS

---
+++ StsServer/Models/AccountViewModels/AccountOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StsServerIdentity.Models.AccountViewModels
+{
+    public class AccountOptions
+    {
+        public static bool AllowLocalLogin = true;
+        public static bool AllowRememberLogin = true;
+        public static TimeSpan RememberMeLoginDuration = TimeSpan.FromDays(30);
+
+        public static bool ShowLogoutPrompt = true;
+        public static bool AutomaticRedirectAfterSignOut = false;
+    }
+}
