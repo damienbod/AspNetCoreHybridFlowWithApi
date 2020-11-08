946c7928f0dd853a43e7b5837001556b863fdc13
damienbod <damien_bod@hotmail.com>
2020-11-08 12:52:13 +0100

    Adding swagger

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System;
 
 namespace WebApi
 {
@@ -60,6 +63,43 @@ namespace WebApi
                 })
             );
 
+            services.AddSwaggerGen(c =>
+            {
+                // add JWT Authentication
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Name = "JWT Authentication",
+                    Description = "Enter JWT Bearer token **_only_**",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer", // must be lower case
+                    BearerFormat = "JWT",
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+                c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {securityScheme, new string[] { }}
+                });
+
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "User API",
+                    Version = "v1",
+                    Description = "User API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "damienbod",
+                        Email = string.Empty,
+                        Url = new Uri("https://damienbod.com/"),
+                    },
+                });
+            });
+
             services.AddControllers(options =>
             {
                 options.Filters.Add(new MissingSecurityHeaders());
@@ -78,6 +118,13 @@ namespace WebApi
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "User API");
+                c.RoutePrefix = string.Empty;
+            });
 
             //Registered before static files to always set header
             app.UseHsts(hsts => hsts.MaxAge(365).IncludeSubdomains());
