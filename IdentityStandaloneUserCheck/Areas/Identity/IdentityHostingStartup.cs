5610ce268120e20a7da5bd16fa86e57c70c0100d
damienbod <damien_bod@hotmail.com>
2021-02-17 22:28:23 +0100

    adding identity

---
+++ IdentityStandaloneUserCheck/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using IdentityStandaloneUserCheck.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IdentityStandaloneUserCheck.Areas.Identity.IdentityHostingStartup))]
+namespace IdentityStandaloneUserCheck.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
