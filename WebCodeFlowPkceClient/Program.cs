d193bf4a1b6942e6ae9b5decb05e93dc32ac65f2
damienbod <damien_bod@hotmail.com>
2021-03-05 13:28:26 +0100

    code clean up

--- WebCodeFlowPkceClient/Program.cs
+++ WebCodeFlowPkceClient/Program.cs
@@ -1,8 +1,5 @@
 using System;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Azure.KeyVault;
-using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
