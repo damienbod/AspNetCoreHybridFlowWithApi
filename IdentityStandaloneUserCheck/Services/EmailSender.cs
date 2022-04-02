be3a389438782d04e3641655d834ec95c9f844ba
damienbod <damien_bod@hotmail.com>
2022-04-02 10:07:41 +0200

    Update packages

--- IdentityStandaloneUserCheck/Services/EmailSender.cs
+++ IdentityStandaloneUserCheck/Services/EmailSender.cs
@@ -5,7 +5,7 @@ namespace IdentityStandaloneUserCheck.Services;
 
 public class EmailSender : IEmailSender
 {
-    public async Task SendEmail(string email, string subject, string message, string toUsername)
+    public Task SendEmail(string email, string subject, string message, string toUsername)
     {
         throw new Exception("not implemented");
     }
--- WebExtraClaimsCached/MyClaimsTransformation.cs
+++ WebExtraClaimsCached/MyClaimsTransformation.cs
@@ -6,7 +6,7 @@ public class MyClaimsTransformation
 {
     public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
     {
-        ClaimsIdentity claimsIdentity = new ClaimsIdentity();
+        var claimsIdentity = new ClaimsIdentity();
         var claimType = "myNewClaim";
         if (!principal.HasClaim(claim => claim.Type == claimType))
         {
