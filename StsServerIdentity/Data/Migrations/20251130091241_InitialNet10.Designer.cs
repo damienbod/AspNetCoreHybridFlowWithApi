7f59dc37527ac907baa6c879c958464dfc56d629
damienbod <damien_bod@hotmail.com>
2025-11-30 10:14:36 +0100

    .NET 10

--- StsServerIdentity/Data/Migrations/20251130091241_InitialNet10.Designer.cs
+++ StsServerIdentity/Data/Migrations/20251130091241_InitialNet10.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IdentityProvider.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IdentityProvider.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251130091241_InitialNet10")]
+    partial class InitialNet10
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.0");
